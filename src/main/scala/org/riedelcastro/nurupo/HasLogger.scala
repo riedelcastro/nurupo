2200bf0065885745576c3d2d4fdf4c97e3899df5
Sebastian Riedel <sebastian.riedel@gmail.com>
2011-08-09 19:09:37 -0400

    initial port from factorieie

---
+++ src/main/scala/org/riedelcastro/nurupo/HasLogger.scala
@@ -0,0 +1,31 @@
+package org.riedelcastro.nurupo
+
+import org.apache.log4j.Logger
+import java.lang.String
+
+
+/**
+ * A HasLogger object has a logger object it can log to. This logger will
+ * be named after the class.
+ * @author sriedel
+ */
+trait HasLogger {
+
+  /**
+   * The logger of this object. Uses the class name as logger name.
+   */
+  val logger = Logger.getLogger(loggerName)
+
+  /**
+   * Returns the name to use for the logger.
+   */
+  def loggerName = getClass.getSimpleName
+
+  /**
+   * creates an object that lazily evaluates the given function when `toString` is called.
+   */
+  def lazyString(func: => String) = new Object {
+    override def toString: String = func
+  }
+
+}
