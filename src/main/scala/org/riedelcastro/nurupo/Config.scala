a84c522c0323b5494aa12eb2866c499f4fcf1479
Sebastian Riedel <sebastian.riedel@gmail.com>
2011-08-30 00:59:22 -0400

    Added Config class

---
+++ src/main/scala/org/riedelcastro/nurupo/Config.scala
@@ -0,0 +1,50 @@
+package org.riedelcastro.nurupo
+
+import java.util.Properties
+import reflect.Manifest
+import java.io.{FileInputStream, File, InputStream}
+
+/**
+ * Very simple configuration file loader based on java properties files.
+ * @author sriedel
+ */
+class Config(val properties: Properties) {
+  def this(in: InputStream) {
+    this ({val props = new Properties; props.load(in); props})
+  }
+
+  def get[T](key: String)(implicit m: Manifest[T]): T = {
+    val value = properties.getProperty(key)
+    if (value == null) error("No value associated with key " + key)
+    createBySimpleErasureName(m.erasure.getSimpleName, value)
+  }
+
+  def get[T](key: String, default: T)(implicit m: Manifest[T]): T = {
+    val value = properties.getProperty(key)
+    if (value == null) default else createBySimpleErasureName(m.erasure.getSimpleName, value)
+  }
+
+  private def createBySimpleErasureName[T](name: String, untrimmed: String): T = {
+    val value = untrimmed.trim
+    val result = name match {
+      case "int" => value.toInt
+      case "String" => value
+      case "double" => value.toDouble
+      case "boolean" => value.toBoolean
+      case "File" => new File(value)
+      case "FileInputStream" => new FileInputStream(value)
+      case x if (x.endsWith("[]")) => {
+        val prefix = x.substring(0, x.indexOf('['))
+        val split = value.split(",").map(_.trim).map(createBySimpleErasureName(prefix, _))
+
+      }
+      case x => error("Can't convert type " + x)
+    }
+    result.asInstanceOf[T]
+  }
+
+  def put[T](key: String, value: T) = {
+    properties.put(key, value.asInstanceOf[Object])
+  }
+
+}
